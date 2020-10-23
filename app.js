6a89af528894a5d33d665254cc3354f8a7d10136
Sayed94h <skazimi951@yahoo.com>
2020-10-23 21:26:34 +0200

    tasks

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
 "use-strict";
 
 // our main application for handling user interaction
-
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-	res.send("Hello");
-});
-
-app.listen(3000, () => console.log("listening to port 3000..."));
