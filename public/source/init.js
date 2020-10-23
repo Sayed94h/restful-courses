469656833b4bb5af5ec4c9997aa95151a9c78ed7
Sayed94h <skazimi951@yahoo.com>
2020-10-23 17:49:45 +0200

    fix a problem with express package name

--- public/source/init.js
+++ public/source/init.js
@@ -1,1 +1,14 @@
 "use-strict";
+
+// const inputVal = document.getElementById("input-todo");
+// const addVal = document.getElementById("add-todo");
+// const todoContainer = document.getElementById("todo-container");
+// const OlEl = document.createElement("ol");
+// function testingAddBtn() {
+// 	const liEl = document.createElement("li");
+// 	liEl.innerHTML = inputVal.value;
+// 	OlEl.appendChild(liEl);
+// 	todoContainer.appendChild(OlEl);
+// }
+
+// addVal.onclick = testingAddBtn;
