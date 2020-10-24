81f790af6f050d4fa3e1c23189d8ad40885b59b6
Feruzteame <teamefitwi@gmail.com>
2020-10-24 09:41:05 +0200

    get

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 "use-strict";
 
 // our main application for handling user interaction
+
+const express = require('express'); // module express
+const app = express(); // call express
+
+//add a piece of middleware (help us to use in request method)
+app.use(express.json())
+
+// Port
+const port = process.env.PORT || 6000 // make port
+app.listen(port, () => console.log(`listen to ${port}..`)) // listen the server
+
+app.get('/basicCourse', (req, res) => {
+
+    const fs = require('fs'); //get file
+    const json = fs.readFileSync(`${__dirname}/data/courses.json`, 'utf-8') // read file
+    const courseData = JSON.parse(json)
+    res.send(courseData)
+
+});
