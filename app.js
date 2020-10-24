b6ef13554f490694256ffa24c4afc7f2b9955911
ali789 <ABDULHAMID.ALI.996@gmail.com>
2020-10-24 12:15:24 +0200

    delete-from json closed #7

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 "use-strict";
 
 // our main application for handling user interaction
+const express = require("express");
+const fs = require("fs");
+const app = express();
+let courses;
+app.get("/", function (req, res) {
+  fs.readFile("./courses.json", "utf-8", (err, data) => {
+    res.send(data);
+  });
+});
+app.get("/:id", function (req, res) {
+  courses = JSON.parse(data);
+  courses = courses.filter((element) => element.id != req.params.id);
+  fs.writeFile("./courses.json", courses, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("course deleted");
+    res.send(" course deleted");
+  });
+});
+app.listen(3000, () => console.log("connecting ......"));
