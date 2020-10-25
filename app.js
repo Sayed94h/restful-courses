861b0fe42f6d337425aea12fb31527c9fcc163d1
feruz teame <59234162+Feruzteame@users.noreply.github.com>
2020-10-25 09:10:53 +0100

    Rearrange the code

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 "use-strict";
 
 // our main application for handling user interaction
+const express = require("express"); // module express
+const app = express(); // call express
 
+//add a piece of middleware (help us to use in request method)
+app.use(express.json());
+
+// Port
+const port = process.env.PORT || 6000; // make port
+app.listen(port, () => console.log(`listen to ${port}..`)); // listen the server
+
+// file system
+const fs = require("fs"); //get file
+const json = fs.readFileSync(`${__dirname}/courses.json`, "utf-8"); // read file
+const courseData = JSON.parse(json);
+
+// read data from file
+app.get("/basicCourse", (req, res) => {
+ res.send(courseData);
+});
+
+
+// delete data from file
 app.get("/:id", function (req, res) {
   courses = JSON.parse(data);
   courses = courses.filter((element) => element.id != req.params.id);
@@ -14,35 +35,6 @@ app.get("/:id", function (req, res) {
   });
 });
 
-const express = require("express"); // module express
-const app = express(); // call express
 
-//add a piece of middleware (help us to use in request method)
-app.use(express.json());
 
-// Port
-const port = process.env.PORT || 6000; // make port
-app.listen(port, () => console.log(`listen to ${port}..`)); // listen the server
 
-app.get("/basicCourse", (req, res) => {
-  const fs = require("fs"); //get file
-  const json = fs.readFileSync(`${__dirname}/courses.json`, "utf-8"); // read file
-  const courseData = JSON.parse(json);
-  res.send(courseData);
-});
-const express = require("express"); // module express
-const app = express(); // call express
-
-//add a piece of middleware (help us to use in request method)
-app.use(express.json());
-
-// Port
-const port = process.env.PORT || 6000; // make port
-app.listen(port, () => console.log(`listen to ${port}..`)); // listen the server
-
-app.get("/basicCourse", (req, res) => {
-  const fs = require("fs"); //get file
-  const json = fs.readFileSync(`${__dirname}/courses.json`, "utf-8"); // read file
-  const courseData = JSON.parse(json);
-  res.send(courseData);
-});
