071ede993df83c96ba57c661f7242eba69ec521d
ali789 <ABDULHAMID.ALI.996@gmail.com>
2020-10-25 08:53:53 +0100

    conflicts resolve

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 
 // our main application for handling user interaction
 
-
-
 app.get("/:id", function (req, res) {
   courses = JSON.parse(data);
   courses = courses.filter((element) => element.id != req.params.id);
@@ -16,3 +14,19 @@ app.get("/:id", function (req, res) {
   });
 });
 
+const express = require("express"); // module express
+const app = express(); // call express
+
+//add a piece of middleware (help us to use in request method)
+app.use(express.json());
+
+// Port
+const port = process.env.PORT || 6000; // make port
+app.listen(port, () => console.log(`listen to ${port}..`)); // listen the server
+
+app.get("/basicCourse", (req, res) => {
+  const fs = require("fs"); //get file
+  const json = fs.readFileSync(`${__dirname}/courses.json`, "utf-8"); // read file
+  const courseData = JSON.parse(json);
+  res.send(courseData);
+});
